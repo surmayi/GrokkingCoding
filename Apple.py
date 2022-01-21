b4c197a9812b362b18d793a32184df9a9d990ec1
Surmayi Shree <sxs190002@utdallas.edu>
2022-01-21 08:39:49 -0600

    Add files via upload

---
+++ Apple.py
@@ -0,0 +1,44 @@
+#https://leetcode.com/problems/number-of-good-pairs/
+def numIdenticalPairs(nums):
+    count = 0
+    for i in range(len(nums) - 1):
+        for j in range(i + 1, len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+    return count
+
+
+print('1. No. of Good pairs: ', numIdenticalPairs([1,2,3,1,1,3]))
+
+
+# https://leetcode.com/problems/decompress-run-length-encoded-list/
+def decompressRLElist(nums):
+    result = []
+    for i in range(0, len(nums), 2):
+        freq, val = nums[i], nums[i + 1]
+        result = result + [val] * freq
+    return result
+
+
+print('2. Decompress Run-Length Encoded List: ', decompressRLElist([1,2,3,1,1,3]))
+
+
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/
+def reverseWords(s) -> str:
+    s = s.strip()
+    if not s:
+        return ''
+    s = s.split()
+    result = []
+    for word in s:
+        temp = []
+        for ch in word:
+            temp.append(ch)
+        while temp:
+            result.append(temp.pop())
+        result.append(' ')
+    result = ''.join(result[:-1])
+    return result
+
+
+print('3. Reverse words in String: ', reverseWords("Let's take LeetCode contest"))
