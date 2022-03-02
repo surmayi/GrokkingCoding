e82e1ba3f7bf8293da1c35d4588ebdc0839723b6
Surmayi Shree <surmayi.shree@ericsson.com>
2022-03-02 12:13:22 -0600

    added

--- GoldmanSachs.py
+++ GoldmanSachs.py
@@ -3417,3 +3417,19 @@ nuts = ['2','5','6','1']
 bolts = ['1','5','6','2']
 compareNutsAndBolts()
 print(nuts, bolts)
+
+
+def minPriceIntervals(intervals):
+    result=[intervals[0]]
+    for i in range(1, len(intervals)):
+        if result[-1][2]>intervals[i][2]:
+            result[-1][1]= intervals[i][0]
+            result.append(intervals[i])
+        else:
+            intervals[i][0]= result[-1][1]
+            result.append(intervals[i])
+    return result
+
+
+intervals=[[0,4,5],[2,8,3],[7,11,10]]
+print('minPriceIntervals', str(minPriceIntervals(intervals)))
